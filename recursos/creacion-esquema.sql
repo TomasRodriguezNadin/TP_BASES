e597dd73c17d2fab30a5703ea79260aac79282e8
Tomas Rodriguez Nadin <tnadin@dc.uba.ar>
2025-10-10 19:36:40 -0300

    SQL para crear las tablas principales

--- recursos/creacion-esquema.sql
+++ recursos/creacion-esquema.sql
@@ -2,16 +2,43 @@ SET ROLE TO tp_owner;
 CREATE SCHEMA tp;
 GRANT USAGE ON SCHEMA TP TO administrador;
 
-CREATE TABLE TP.alumnos (
-    lu TEXT PRIMARY KEY,
+CREATE TYPE experiencia AS ENUM ('principiante', 'mediano', 'experimentado');
+
+CREATE TABLE TP.escribanos (
+    matricula INT PRIMARY KEY,
     nombre TEXT NOT NULL,
     apellido TEXT NOT NULL,
-    titulo TEXT NOT NULL,
-    titulo_en_tramite TEXT,
-    egreso date
+    capacidad experiencia NOT NULL
 );
 
-grant SELECT, INSERT, UPDATE, DELETE on TP.alumnos to administrador;
-grant SELECT, INSERT, UPDATE, DELETE on TP.usuarios to administrador;
-GRANT USAGE, SELECT ON SEQUENCE tp.usuarios_id_seq TO administrador;
-GRANT UPDATE ON SEQUENCE tp.usuarios_id_seq TO administrador;
+grant SELECT, INSERT, UPDATE, DELETE on TP.escribanos to administrador;
+
+CREATE TABLE TP.clientes (
+    cuit INT PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    apellido TEXT NOT NULL
+);
+
+grant SELECT, INSERT, UPDATE, DELETE on TP.clientes to administrador;
+
+CREATE TABLE TP.tipoEscrituras (
+    idTipo INT PRIMARY KEY,
+    tipo TEXT,
+    experienciaRequerida experiencia NOT NULL
+);
+
+grant SELECT, INSERT, UPDATE, DELETE on TP.tipoEscrituras to administrador;
+
+CREATE TABLE TP.escrituras (
+    matricula INT PRIMARY KEY
+    nroProtocolo INT,
+    anio INT,
+    idTipo INT,
+    cuit INT,
+    PRIMARY KEY (matricula, nroProtocolo, anio),
+    FOREIGN KEY (matricula) REFERENCES TP.escribanos(matricula),
+    FOREIGN KEY (idTipo) REFERENCES TP.tipoEscrituras(idTipo),
+    FOREIGN KEY (cuit) REFERENCES TP.clientes(cuit)
+);
+
+grant SELECT, INSERT, UPDATE, DELETE on TP.escrituras to administrador;
