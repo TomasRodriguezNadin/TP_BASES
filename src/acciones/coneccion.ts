22d47f6641fae80f3d7ad3ce7cdc9d7ecb897f5e
Tomas Rodriguez Nadin <tnadin@dc.uba.ar>
2025-10-29 18:35:51 -0300

    Agrego la variable de entorno para saber si estamos en local o produccion y ahora se conecta de forma acorde

--- src/acciones/coneccion.ts
+++ src/acciones/coneccion.ts
@@ -1,7 +1,8 @@
 import { Client } from 'pg';
 
-export async function crearCliente(): Client{
-    const clientDB = new Client();
+export async function crearCliente(): Promise<Client>{
+    const clientDB = process.env.DB == 'Local' ? new Client()
+                    : new Client({ connectionString: process.env.DATABASE_URL });
     await clientDB.connect();
     return clientDB;
 }
