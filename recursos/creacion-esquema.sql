5cc5561647fb5c5ded062ccbe68f511e1b4cb733
Damy1928 <damyortiz19@gmail.com>
2025-10-29 10:30:40 -0300

    Se puede despedir escribanos, no se puede contratar escribanos no contratados y algunos bug fixes en la creacion de tablas

--- recursos/creacion-esquema.sql
+++ recursos/creacion-esquema.sql
@@ -4,11 +4,15 @@ GRANT USAGE ON SCHEMA TP TO administrador;
 
 CREATE TYPE experiencia AS ENUM ('principiante', 'mediano', 'experimentado');
 
+CREATE TYPE estado AS ENUM ('contratado', 'no contratado');
+
+
 CREATE TABLE TP.escribanos (
     matricula INT PRIMARY KEY,
     nombre_escribano TEXT NOT NULL,
     apellido TEXT NOT NULL,
-    capacidad experiencia NOT NULL
+    capacidad experiencia NOT NULL,
+    estado estado DEFAULT 'contratado'
 );
 
 grant SELECT, INSERT, UPDATE, DELETE on TP.escribanos to administrador;
@@ -27,17 +31,17 @@ CREATE TABLE TP.tipo_escrituras (
     experiencia_requerida experiencia NOT NULL
 );
 
-grant SELECT, INSERT, UPDATE, DELETE on TP.tipoEscrituras to administrador;
+grant SELECT, INSERT, UPDATE, DELETE on TP.tipo_escrituras to administrador;
 
 CREATE TABLE TP.escrituras (
     matricula INT,
     nro_protocolo INT,
     anio INT,
-    idTipo INT,
+    id_tipo INT,
     cuit BIGINT,
     PRIMARY KEY (matricula, nro_protocolo, anio),
     FOREIGN KEY (matricula) REFERENCES TP.escribanos(matricula),
-    FOREIGN KEY (id_tipo) REFERENCES TP.tipoEscrituras(id_tipo),
+    FOREIGN KEY (id_tipo) REFERENCES TP.tipo_escrituras(id_tipo),
     FOREIGN KEY (cuit) REFERENCES TP.clientes(cuit)
 );
 
--- recursos/creacion-trigger.sql
+++ recursos/creacion-trigger.sql
@@ -7,7 +7,26 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION TP.verificar_escribano_contratado()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NOT EXISTS(
+        SELECT 1
+        FROM TP.escribanos e
+        WHERE e.matricula = NEW.matricula AND e.estado = 'contratado') THEN
+            RAISE EXCEPTION 'El escribano % no esta contratado', NEW.matricula;
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TRIGGER eliminar_escrituras_automaticamente
 AFTER INSERT OR UPDATE of anio ON tp.escrituras
 FOR EACH STATEMENT
 EXECUTE FUNCTION tp.eliminar_escrituras_viejas();
+
+CREATE TRIGGER verificar_escribano_contratado
+BEFORE INSERT OR UPDATE ON TP.escrituras
+FOR EACH ROW
+EXECUTE FUNCTION TP.verificar_escribano_contratado();
